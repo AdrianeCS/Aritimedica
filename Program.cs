b938a86d1a6f930881c4e9a55a22b1ea5e2938ad
adriane <adrianedacosta21@gmail.com>
2022-03-25 22:05:30 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double primeiroNumero, segundoNumero, terceiroNumero, media;
+Console.WriteLine(" media aritimedica de 3 numeros");
+
+Console.Write("digite o primeiro numero: ");
+primeiroNumero=Convert.ToDouble(Console.ReadLine());
+
+Console.Write("digite o segundo numero: ");
+segundoNumero=Convert.ToDouble(Console.ReadLine());
+
+Console.Write("digite o terceiro numero: ");
+terceiroNumero=Convert.ToDouble(Console.ReadLine());
+
+media=(primeiroNumero + segundoNumero + terceiroNumero) /3;
+
+Console.WriteLine($"\nMédia: {media:N1}");
